07276d078a7c8952a30a6b2e5c42112a7d1c3157
Sinjoy Saha <sinjoysaha555@gmail.com>
2020-12-26 15:44:38 +0530

    Adding js/app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function translateEng2Morse() {
+    var inputtext =  document.getElementById("inputtext").value;
+    var outputtext =  document.getElementById("outputtext");
+
+    outputtext.innerHTML = inputtext;
+}
