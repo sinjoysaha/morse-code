9709e3a29ad0234c92c5390b4951e9860b8ed523
Sinjoy Saha <sinjoysaha555@gmail.com>
2020-12-28 13:58:07 +0530

    Adding translate() function in app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
+var code = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+        'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+        'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+        'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+        '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '.': '.-.-.-', ',': '--..--',
+        '?': '..--..', '=': '-...-'};
+
 function translateEng2Morse() {
     var inputtext =  document.getElementById("inputtext").value;
-    var outputtext =  document.getElementById("outputtext");
+    var outputtext =  document.getElementById("outputtext");    
+    
+    var result = translate(inputtext);
 
-    outputtext.innerHTML = inputtext;
+    outputtext.innerHTML = result;
+}
+
+function translate(inputtext) {
+    var str = inputtext.toUpperCase();
+    var ss = "";
+    for (var i = 0; i < str.length; i++) {
+        if(str.charAt(i)==' ')
+        ss = ss + "| ";
+        else
+        ss = ss + code[str.charAt(i)] + " ";
+    }
+    return ss
 }
